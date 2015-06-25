7f6dc0cf1c14d9882dde6c4f0b5494cead725d6b
Vincent Pham <vpham@quantcast.com>
2015-06-25 12:33:29 -0700

    Half-working front page

--- dinnertime/views/frontend.py
+++ dinnertime/views/frontend.py
@@ -1,8 +1,24 @@
 # -*- encoding:utf-8 -*-
 from flask import Blueprint
+from flask import Flask, render_template, request
+import os
 
 frontend = Blueprint('frontend',__name__)
 
-@frontend.route('/')
+@frontend.route('/', methods=['GET', 'POST'])
 def index():
-    return "Hello,world"
+    errors = []
+    results = {}
+    url = 'https://www.quantcast.com/'
+    if request.method == "POST":
+        # get url that the user has entered
+        try:
+            url = request.form['url']
+        except:
+            errors.append(
+                "Unable to get URL. Please make sure it's valid and try again."
+            )
+    print url
+    return render_template('index.html', iframe=url)
+
+
